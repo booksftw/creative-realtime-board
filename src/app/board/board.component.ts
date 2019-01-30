0d5c1325898d443f88b06a3d39c5faefda278016
Nicholas Zachary <nick@Nicholass-MacBook-Air.local>
2019-01-29 23:03:03 -0800

    changing branch

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -15,8 +15,6 @@ import * as firebase from '../../../node_modules/firebase'
 })
 export class BoardComponent implements OnInit, AfterContentInit {
   db
-  all_ids_in_view = {}
-  registeredIds = {}
   @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef
 
   constructor(
@@ -31,52 +29,11 @@ export class BoardComponent implements OnInit, AfterContentInit {
 
   testGenerateComponent(id) {
     console.log('test generating componenet')
-
-    // const stickyNoteFactory = this.resolver.resolveComponentFactory(StickyNoteComponent)
-    // const component = this.entry.createComponent(stickyNoteFactory)
-    // component.instance.stickyId = 2132132132
   }
 
   testGenerateStickyNote(componentId?) {
-    if (!componentId) {
-      // Create new block
-      //
-      componentId = this.boardUtil.getRandomId()
-      this.all_ids_in_view[componentId] = true
-
-      // Register the updated view state online
-      this.db.child('room').child('0').child(`registered_blocks`).child(`${componentId}`).update({
-        id: componentId,
-        content: 'generated test',
-        type: 'sticky-note'
-      })
-
-      // Create the component and pass data down
-      const stickyNoteFactory = this.resolver.resolveComponentFactory(StickyNoteComponent)
-      const component = this.entry.createComponent(stickyNoteFactory)
-      component.instance.test = 'CREATES NEW STICKY'
-      component.instance.stickyId = componentId
-    } else {
-      // Render Existing block
-      //
-      console.log('Render exisiting block. Component Id:', componentId)
-
-      const stickyNoteFactory = this.resolver.resolveComponentFactory(StickyNoteComponent)
-      const component = this.entry.createComponent(stickyNoteFactory)
-
-      console.log('component instance', component.instance)
-      component.instance.test = 'Howdy THIS CODE RENDERS EXISTING'
-      component.instance.stickyId = componentId
-
-
-      this.db.child('room').child('0').child(`registered_blocks`).child(`${componentId}`).update({
-        id: componentId,
-        content: 'generated test',
-        type: 'sticky-note'
-      })
-
-    }
-
+    console.log('generate sticky')
+    componentId? componentId = componentId : componentId = this.boardUtil.getRandomId()
       this.db = firebase.database().ref()
       // set intial position
       this.db.child('room').child('0').child(`blocks/${componentId}`).set({
@@ -92,11 +49,6 @@ export class BoardComponent implements OnInit, AfterContentInit {
 
   ngOnInit() {
 
-    // this.testGenerateComponent('1111111')
-    // const stickyNoteFactory = this.resolver.resolveComponentFactory(StickyNoteComponent)
-    // const component = this.entry.createComponent(stickyNoteFactory)
-    // component.instance.stickyId = 21321321321321
-
     // ~ Render the blocks and their state from firebase
     this.db = firebase.database().ref()
 
@@ -109,7 +61,6 @@ export class BoardComponent implements OnInit, AfterContentInit {
           const blockType = el.type
           const blockContent = el.content
 
-          this.all_ids_in_view[blockId] = true
           this.testGenerateStickyNote(blockId)
           // const block = this.boardUtil.generateComponent(blockType, blockContent)
           // this.renderDraggableToPage(block, blockId)
@@ -119,87 +70,12 @@ export class BoardComponent implements OnInit, AfterContentInit {
 
 
     this.db.child('room').child('0').child('registered_blocks').on('value', (snapshot) => {
-      const blocksSet = snapshot.val()
-      for (const key in blocksSet) {
-        if (blocksSet.hasOwnProperty(key)) {
-          const el = blocksSet[key]
-          const blockId = el.id
-          const blockType = el.type
-          const blockContent = el.content
-          const all_registered_ids = []
-
-          this.db.child('room').child('0').child('registered_blocks').once('value').then((snap) => {
-
-            const allRegistered = snap.val()
-            for (const keyReg of Object.keys(allRegistered)) {
-                all_registered_ids.push(allRegistered[keyReg].id)
-            }
-
-            const all_ids_in_view_array = Object.keys(this.all_ids_in_view)
-            const all_view_ids = this.boardUtil.convertArrayOfStringsToNumber(all_ids_in_view_array)
-
-            const pending_view_ids = this.boardUtil.getDifferenceBetweenArrays(all_view_ids, all_registered_ids)
-            // IF view ids different form register ids fill in the view ids and stop.
-            const isViewAndRegisterSync = this.boardUtil.checkArraysAreEqual(all_view_ids, all_registered_ids)
-            
-            console.log('registered block fired')
-            if (!isViewAndRegisterSync) {
-              pending_view_ids.forEach(id => {
-                this.all_ids_in_view[id] = true
                 // Generate component with the ids
-                this.testGenerateStickyNote(id)
-              })
-            }
+                // this.testGenerateStickyNote(id)
 
-          })
-
-        }
-      }
     })
 
   }
 
-  ngAfterContentInit(): void {
-
-    // this.db.child('room').child('0').child('registered_blocks').on('value', (snapshot) => {
-    //   const blocksSet = snapshot.val()
-    //   for (const key in blocksSet) {
-    //     if (blocksSet.hasOwnProperty(key)) {
-    //       const el = blocksSet[key]
-    //       const blockId = el.id
-    //       const blockType = el.type
-    //       const blockContent = el.content
-    //       const all_registered_ids = []
-
-    //       this.db.child('room').child('0').child('registered_blocks').once('value').then((snap) => {
-
-    //         const allRegistered = snap.val()
-    //         for (const keyReg of Object.keys(allRegistered)) {
-    //             all_registered_ids.push(allRegistered[keyReg].id)
-    //         }
-    //         console.log('allRegistered has', all_registered_ids.length, 'values')
-
-    //         const all_ids_in_view_array = Object.keys(this.all_ids_in_view)
-    //         const all_view_ids = this.boardUtil.convertArrayOfStringsToNumber(all_ids_in_view_array)
-
-    //         console.log('BoardComponent: all-view-ids', all_view_ids, 'all-register-ids', all_registered_ids)
-    //         // ! Get array of ids that arent on the view board and render them
-    //         const pending_view_ids = this.boardUtil.getDifferenceBetweenArrays(all_view_ids, all_registered_ids)
-    //         console.log('pending_view_ids:', pending_view_ids)
-
-    //         pending_view_ids.forEach( id => {
-    //           // Generate component with the ids
-    //           this.testGenerateStickyNote(id)
-    //           // this.testGenerateComponent(id)
-    //         })
-    //       })
-
-    //     }
-    //   }
-    // })
-
-  }
-
-
-
+  ngAfterContentInit(): void { }
 }
--- src/app/board/sticky-note/sticky-note.component.ts
+++ src/app/board/sticky-note/sticky-note.component.ts
@@ -1,13 +1,15 @@
-import { Component, AfterContentInit, ElementRef, Renderer2 } from '@angular/core'
+import { AngularFireDatabase } from '@angular/fire/database'
+import { Component, AfterContentInit, ElementRef, Renderer2, OnInit, ViewChild      } from '@angular/core'
 import { DragService } from './../../shared/drag.service'
 import * as $ from 'jquery'
+import {catchError, flatMap} from 'rxjs/operators'
 
 @Component({
   selector: 'app-sticky-note',
   templateUrl: './sticky-note.component.html',
   styleUrls: ['./sticky-note.component.css']
 })
-export class StickyNoteComponent implements AfterContentInit {
+export class StickyNoteComponent implements OnInit {
 
   test = 'Default sticky text'
   stickyId = 1
@@ -15,13 +17,16 @@ export class StickyNoteComponent implements AfterContentInit {
   constructor(
     private drag: DragService,
     private el: ElementRef,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private db: AngularFireDatabase,
   ) { }
 
-  ngAfterContentInit(): void {
-    const stickyNote = this.el.nativeElement.querySelector('div')
-    this.drag.dragElement(stickyNote)
-
-    // this.resizable(stickyNote)
+  ngOnInit() {
+    // const stickyNote = this.el.nativeElement.querySelectorAll('div')[0]
+    // this.drag.dragElement(stickyNote)
   }
+
+
+
+
 }
